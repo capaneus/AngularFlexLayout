8d0ddb1d671707998198d86dcc9e6addb87a3b2e
Dmitry Egorov (DevExpress) <egorov.dmitry@corp.devexpress.com>
2018-04-05 01:29:00 +0300

    Responsive layout added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FlexLayoutModule, BreakPoint, BREAKPOINTS, CUSTOM_BREAKPOINTS_PROVIDER_FACTORY } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 
+const CUSTOM_BREAKPOINTS: BreakPoint[] = [{
+  alias: 'lg',
+  mediaQuery: '(min-width: 700px)',
+}, {
+  alias: 'sm',
+  mediaQuery: '(max-width: 700px)',
+}];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule
   ],
-  providers: [],
+  providers: [CUSTOM_BREAKPOINTS_PROVIDER_FACTORY(CUSTOM_BREAKPOINTS, { defaults: true })],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
